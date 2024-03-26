c131a09eb345a60b6c6dbd491ffe41a6952debb6
Tanny <1441049296@qq.com>
2024-03-26 12:16:38 +0800

    chore:init project

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  vueIndentScriptAndStyle: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  proseWrap: 'always',
+  htmlWithWhitespaceSensitivity: 'strict',
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-packagejson'],
+  overrides: [{
+    files: '.*rc',
+    options: {
+      parser: 'json',
+    }
+  }]
+}
